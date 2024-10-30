083a0c6a06ae6e8d29b5873f4e4694391ad9a96f
malik LAFIA <maliksero.lafia@gmail.com>
2024-10-30 15:22:13 +0100

    configuration de la bd1

--- db.js
+++ db.js
@@ -1,23 +1,19 @@
 const { Sequelize } = require("sequelize");
 
 // Database
-const sequelize = new Sequelize(
-  (process.env.DATABASE_URL =
-    "postgresql://esgi_for_render_db_user:9Hwo7pHHUVRPwMbDfEnUdlCD87Bsq1uy@dpg-csh3odggph6c73bvqme0-a/esgi_for_render_db"),
-  {
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    define: {
-      createdAt: "added",
-      updatedAt: "updated",
-    },
-  }
-);
+  },
+  define: {
+    createdAt: "added",
+    updatedAt: "updated",
+  },
+});
 
 sequelize.authenticate();
 sequelize.sync();
